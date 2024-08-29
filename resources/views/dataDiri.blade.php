c1eaaada775caddc8d7b20e441672735cb1e4666
Maritza Ulfa Huriyah <maritza.ulfa.huriyah@gmail.com>
2024-08-29 18:09:39 +0700

    Pertemuan 1

---
+++ resources/views/dataDiri.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>KTP Risa</title>
+    <link rel="stylesheet" href="styleKTP.css">
+    <style>
+        
+        .kartu {
+            border: 1px solid black;
+            margin: auto;
+            width: 700px;
+            height: 480px;
+            border-radius: 20px;
+            background-size: cover;
+            position: relative;
+        }
+
+        .judul {
+            width: 100%;
+            text-align: center;
+            font-weight: bold;
+            position: absolute;
+            bottom: 380px;
+            font-size: 19px;
+        }
+
+        .isi {
+            position: absolute;
+            top: 120px;
+            left: 70px;
+            width: 400px;
+            font-size: 16px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="kartu">
+        <div class="judul">
+            <table>
+                <p>PROVINSI PAPUA</p>
+                <p>KOTA JAYAPURA</p>
+            </table>
+        </div>
+        <div class="isi">
+            <table cellpadding="1" cellspacing="">
+                <tr>
+                    <td style="font-size: large;font-weight: bold;">NIK</td>
+                    <td style="font-size: large;font-weight: bold;"> : 91710*****</td>
+                </tr>
+                <tr>
+                    <td>Nama</td>
+                    <td> : MARITZA ULFA HURIYAH</td>
+                </tr>
+                <tr>
+                    <td>Tempat/Tgl Lahir</td>
+                    <td> : JAYAPURA, 19-10-2004</td>
+                </tr>
+                <tr>
+                    <td>Jenis Kelamin</td>
+                    <td>: PEREMPUAN</td>
+                </tr>
+                <tr>
+                    <td>Golongan Darah</td>
+                    <td> : A</td>
+                </tr>
+                <tr>
+                    <td>Alamat</td>
+                    <td> : PEMDA III MELATI KOTARAJA</td>
+                </tr>
+                <tr>
+                    <td>RT/RW</td>
+                    <td> : 003/004</td>
+                </tr>
+                <tr>
+                    <td>Kel/Desa</td>
+                    <td> : WAI MHOROCK</td>
+                </tr>
+                <tr>
+                    <td>Kecamatan</td>
+                    <td> : ABEPURA</td>
+                </tr>
+                <tr>
+                    <td>Agama</td>
+                    <td> : ISLAM</td>
+                </tr>
+                <tr>
+                    <td>Status Perkawinan</td>
+                    <td> : BELUM KAWIN</td>
+                </tr>
+                <tr>
+                    <td>Pekerjaan</td>
+                    <td> : PELAJAR/MAHASISWA</td>
+                </tr>
+                <tr>
+                    <td>Kewarganegaraan</td>
+                    <td> : WNI</td>
+                </tr>
+                <tr>
+                    <td>Berlaku Hingga</td>
+                    <td> : SEUMUR HIDUP</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <br>
+</body>
+
+</html>
